ee9ded3f8fb048e8b0795833cc16a2913dd5e75e
Jeddy <jeddyhammond@gmail.com>
2025-09-21 22:44:14 +0000

    finished work

--- github-user-search/vite.config.js
+++ github-user-search/vite.config.js
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Vercel expects the build output here
-  },
+  base: './', // 👈 ensures Vercel serves correctly
 })
