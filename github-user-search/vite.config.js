1b272d8f4265552915a2f220f0f4458b3f452bcb
Jeddy <jeddyhammond@gmail.com>
2025-09-25 15:06:37 +0000

    Initial commit

--- github-user-search/vite.config.js
+++ github-user-search/vite.config.js
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // 👈 ensures Vercel serves correctly
-})
+  build: {
+    outDir: "dist", // 👈 important, Vite’s default
+  },
+});
