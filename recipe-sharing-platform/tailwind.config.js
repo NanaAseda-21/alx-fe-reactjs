fb8972193321ce8bbd38229e51a7d27829655d18
Jeddy <jeddyhammond@gmail.com>
2025-10-05 22:22:13 +0000

    module.exports added

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,6 +1,8 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+ module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // You can set it to 'media' or 'class' if needed
+  theme: {
+    extend: {},
   },
-}
+  plugins: [],
+};
