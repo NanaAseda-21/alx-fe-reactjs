08d95991807b61fb1bdde5a4a5392315fd9a7ec2
Jeddy <jeddyhammond@gmail.com>
2025-10-19 23:21:25 +0000

    corrected 3rd bug

--- react-todo/eslint.config.js
+++ react-todo/eslint.config.js
@@ -26,4 +26,17 @@ export default defineConfig([
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
     },
   },
+
+  {
+  "env": {
+    "browser": true,
+    "es2021": true,
+    "jest": true
+  },
+  "extends": [
+    "react-app",
+    "react-app/jest"
+  ]
+}
+
 ])
