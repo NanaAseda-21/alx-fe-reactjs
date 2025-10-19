80fc336ff769b7a58f45378c0dd4546f072ab3f1
Jeddy <jeddyhammond@gmail.com>
2025-10-19 21:45:43 +0000

    corrected bug

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -1,21 +1,13 @@
 import { useState } from "react";
 
 function RegistrationForm() {
-  const [formData, setFormData] = useState({
-    username: "",
-    email: "",
-    password: "",
-  });
-
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [error, setError] = useState("");
-
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const { username, email, password } = formData;
 
     if (!username || !email || !password) {
       setError("All fields are required!");
@@ -23,12 +15,12 @@ function RegistrationForm() {
     }
 
     setError("");
-    console.log("Form Submitted:", formData);
+    console.log("Form Submitted:", { username, email, password });
 
     // Simulate API request
     fetch("https://jsonplaceholder.typicode.com/users", {
       method: "POST",
-      body: JSON.stringify(formData),
+      body: JSON.stringify({ username, email, password }),
       headers: { "Content-Type": "application/json" },
     })
       .then((res) => res.json())
@@ -38,30 +30,31 @@ function RegistrationForm() {
 
   return (
     <div style={{ maxWidth: "400px", margin: "2rem auto" }}>
-      <h2>User Registration (Controlled Form)</h2>
+      <h2>User Registration (Controlled Components)</h2>
+
       <form onSubmit={handleSubmit}>
         <label>Username:</label>
         <input
           type="text"
           name="username"
-          value={formData.username}
-          onChange={handleChange}
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
         />
 
         <label>Email:</label>
         <input
           type="email"
           name="email"
-          value={formData.email}
-          onChange={handleChange}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
 
         <label>Password:</label>
         <input
           type="password"
           name="password"
-          value={formData.password}
-          onChange={handleChange}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
 
         {error && <p style={{ color: "red" }}>{error}</p>}
